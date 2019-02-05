9138a63f468d747c037a638085020f2a3467b29b
Thanapat <thanapat423@gmail.com>
2019-02-05 08:39:22 +0700

    Initial Commit

---
+++ PrivatePodLocalize/PrivatePodLocalize.h
@@ -0,0 +1,19 @@
+//
+//  PrivatePodLocalize.h
+//  PrivatePodLocalize
+//
+//  Created by Thanapat Sorralump on 5/2/2562 BE.
+//  Copyright © 2562 Thanapat Sorralump. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrivatePodLocalize.
+FOUNDATION_EXPORT double PrivatePodLocalizeVersionNumber;
+
+//! Project version string for PrivatePodLocalize.
+FOUNDATION_EXPORT const unsigned char PrivatePodLocalizeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrivatePodLocalize/PublicHeader.h>
+
+
