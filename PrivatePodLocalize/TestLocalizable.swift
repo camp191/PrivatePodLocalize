7bb15edc141046931710f6084de57423ecaa0a68
Thanapat <thanapat423@gmail.com>
2019-02-05 08:54:39 +0700

    Update test pod

---
+++ PrivatePodLocalize/TestLocalizable.swift
@@ -0,0 +1,11 @@
+//
+//  TestLocalizable.swift
+//  PrivatePodLocalize
+//
+//  Created by Thanapat Sorralump on 5/2/2562 BE.
+//  Copyright © 2562 Thanapat Sorralump. All rights reserved.
+//
+
+import Foundation
+
+open class TestLocalizable {}
